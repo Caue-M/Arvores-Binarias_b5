d0c6b16cbda7d28055df737ec119cb9a19604718
Caue-M <caue@edu.unifil.br>
2025-06-21 17:29:06 -0300

    Versionamento - Inserção de testes na main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-
-        //Teste para exercício 2, contar nós
-        No no = new No("A");
+        //Testes de contagem
         Contador contador = new Contador();
-        System.out.println(contador.contar(no));
+        No noA = new No("A");
+        No noB = new No("B");
+        No noC = new No("C");
+        No noD = new No("D");
+        No noE = new No("E");
+        //Definição da estrutura
+        noA.esquerda = noB;
+        noA.direita = noC;
+        noB.esquerda = noD;
+        noB.direita = noE;
+
+
+        System.out.println("Contador de nós: ");
+        System.out.println(contador.contar(noA));
+        System.out.println("Exibição em pré ordem: ");
+        contador.contarPreOrdem(noA);
     }
 }
