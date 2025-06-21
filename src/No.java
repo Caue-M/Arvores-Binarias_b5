cdd364bae9dea42f8c4ec8d4ccefabb79c24130f
caue-M <caue@edu.unifil.br>
2025-06-21 16:24:33 -0300

    EX 1 - Versionamento

---
+++ src/No.java
@@ -0,0 +1,12 @@
+public class No {
+    String valor;
+    No direita;
+    No esquerda;
+
+    public No(String valor){
+        this.valor = valor;
+        direita = null;
+        esquerda = null;
+    }
+
+}
